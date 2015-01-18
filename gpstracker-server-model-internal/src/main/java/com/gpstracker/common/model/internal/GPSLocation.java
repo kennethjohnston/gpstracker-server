926760e8f63d6c7756f6ffb8d82b55dbdb7bc8b7
kennethjohnston <kower007@hotmail.com>
2015-01-18 17:20:30 +0000

    testing

---
+++ gpstracker-server-model-internal/src/main/java/com/gpstracker/common/model/internal/GPSLocation.java
@@ -0,0 +1,39 @@
+package com.gpstracker.common.model.internal;
+
+/**
+ * 
+ */
+public class GPSLocation {
+	private Double longitude;
+	private Double latitude;
+	
+	/**
+	 * @return the longitude
+	 */
+	public Double getLongitude() {
+		return longitude;
+	}
+	
+	/**
+	 * @param longitude the longitude to set
+	 */
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+	
+	/**
+	 * @return the latitude
+	 */
+	public Double getLatitude() {
+		return latitude;
+	}
+	
+	/**
+	 * @param latitude the latitude to set
+	 */
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	
+}
